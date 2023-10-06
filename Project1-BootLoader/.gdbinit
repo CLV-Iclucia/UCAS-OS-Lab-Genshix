cc63849b65b14f791996da39187bcd41f3521510
CreeperLordVader-Iclucia-Ashina <1749065611@qq.com>
2023-10-06 18:47:15 +0800

    P1 done

--- Project1-BootLoader/.gdbinit
+++
@@ -1,3 +0,0 @@
-target remote:1234
-tui enable
-layout asm
--- Project1-BootLoader/test/test_project1/mytest.c
+++
@@ -1,9 +0,0 @@
-#include <kernel.h>
-
-int s[4] = {1, 2, 4, 3};
-int main() {
-  if (s[0] != 1 || s[1] != 2 || s[2] != 4 || s[3] != 3) {
-      bios_putstr("fail!\n\r");
-  }
-  return 0;
-}
